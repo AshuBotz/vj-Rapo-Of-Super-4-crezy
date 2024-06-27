70d9a71aa60ff6b29a294cbd8a87e4e6663535c4
Sonu dhakad <145421908+Sonudhakad786@users.noreply.github.com>
2024-06-27 14:51:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -36,6 +36,9 @@ from database.gfilters_mdb import (
     get_gfilters,
     del_allg
 )
+import os
+req_channel = int(os.environ.get('UPDATE CHANNEL','100209055998'))
+
 import logging
 from urllib.parse import quote_plus
 from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
